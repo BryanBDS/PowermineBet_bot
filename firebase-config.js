87f4f33cfb5b4a30404ba37e2495c81099d64ed6
BryanBDS <brayansrz.2018@gmail.com>
2026-02-21 08:26:14 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,18 @@
 // firebase-conf
 // firebase-config.js
 // Configuración de Firebase - ¡REEMPLAZA CON TUS DATOS!
+// firebase-config.js
+
 const firebaseConfig = {
-    apiKey: "AIzaSyA-dKqUnN2-Czy4OU0j66h4FmRn8qRNfoI",
+    apiKey: "7866575006:AAGXX3I9UtB60UHAc4IWuEI6UZZUFKlH22w",
     authDomain: "powerminebet-bot.firebaseapp.com",
-    databaseURL: "https://powerminebet-bot-default-rtdb.firebaseio.com",
     projectId: "powerminebet-bot",
-    storageBucket: "powerminebet-bot.firebasestorage.app",
+    storageBucket: "powerminebet-bot.appspot.com",
     messagingSenderId: "240407528472",
     appId: "1:240407528472:web:fd579ee5bcf211bf20af6d"
-    };
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+};
 
-    //obteneruna referencia ala base de datos
-    const database = firebase.database();
+firebase.initializeApp(firebaseConfig);
+
+// Inicializar Firestore
+const db = firebase.firestore();
