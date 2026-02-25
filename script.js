6a79e5d29d4b52078a9704b88244596f0913a0ee
BryanBDS <brayansrz.2018@gmail.com>
2026-02-24 19:12:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -339,7 +339,7 @@ async function startMining() {
     balance: firebase.firestore.FieldValue.increment(ganancia),
     energy: firebase.firestore.FieldValue.increment(-1),
     lastUpdate: firebase.firestore.FieldValue.serverTimestamp()
-
+    });
     }, 5000);
 }
 
@@ -516,6 +516,7 @@ async function buyBuilding(key) {
 window.buyUpgrade = buyUpgrade;
 window.withdraw = withdraw;
 window.buyBuilding = buyBuilding;
+
 
 
 
